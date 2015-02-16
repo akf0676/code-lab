b4949e15860e11f6d930923c92efdc4a0e8eae0b
Andy Farmer <Andy.Farmer@specialistholidays.com>
2015-02-16 10:38:42 +0000

    Adjusted the Grunt and Package.json files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,122 @@
+// 
+/*!
+ * Akf0676 Gruntfile.js
+ * https://github.com/akf0676/code-lab
+ * @author Andy Farmer
+ * 
+ * @description our wrapper function (required by grunt and its plugins) all configuration goes inside this function
+ * 
+ */
+'use strict';
+/**
+ * Grunt Module
+ */
 module.exports = function(grunt) {
 
-  // Project configuration.
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
   grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+
+    /**
+     * Set project object
+     */
+    project: {
+    	  app: 'app',
+    	  assets: '<%= project.app %>/assets',
+    	  src: 'src',
+    	  css: [
+    	    '<%= project.src %>/scss/style.scss'
+    	  ],
+    	  js: [
+    	    '<%= project.src %>/js/*.js'
+    	  ]
+    },
+    /**
+     * Project banner
+     */
+    tag: {
+    	  banner: 'hello'
+    	},
+    // all of our configuration will go here
+    	/**
+    	 * Sass
+    	 */
+    	sass: {
+    	  dev: {
+    	    options: {
+    	      style: 'expanded',
+    	      sourcemap: 'none',
+    	    },
+    	    files: {
+    	      'css/style.css': '<%= project.css %>'
+    	    }
+    	  },
+    	  dist: {
+    	    options: {
+    	      style: 'compressed',
+    	      sourcemap: 'none',
+    	    },
+    	    files: {
+    	      'css/style.min.css': '<%= project.css %>'
+    	    }
+    	  }
+    	},
+    /**
+     * Watch
+     */
+    watch: {
+      sass: {
+        files: '<%= project.src %>/Scss/{,*/}*.{scss,sass}',
+        tasks: ['sass:dev']
       }
-    }
+    },
+    // configure jshint to validate js files -----------------------------------
+	jshint: {
+	  options: {
+	    reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+	  },
+	
+	  // when this task is run, lint the Gruntfile and all js files in src
+	  build: ['Gruntfile.js', '<%= project.js %>']
+	},
+  
+	// Uglify
+	uglify: {
+		options: {
+	      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		},
+		 build: {
+	        files: {
+	          'js/<%= pkg.name %>.min.js': ['src/js/magic.js', 'src/js/magic2.js']
+	        }
+	      }
+	 }  	
+  
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'sass:dev',
+    'watch'
+  ]);
 
 };
